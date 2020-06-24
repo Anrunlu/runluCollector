185cba9635de79b092c13e26a8d5c8968fe37e03
anrunlu <anrunlu@163.com>
2020-06-24 20:40:17 +0800

    init

---
+++ server/apps/admin/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DbModule } from '@libs/db';
+
+@Module({
+  imports: [
+    DbModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
